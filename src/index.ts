32b43d123b855f7637c17de84305dd183598ebaf
ViniciusMdeiros <Vinicius-medeiros2011@hotmail.com>
2023-02-25 17:48:38 -0300

    Second commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import SwaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 
 export { app };
